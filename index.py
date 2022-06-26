79e60eec4a307431a9c5d741227633ace9f94b34
Vombit <45901675+Vombit@users.noreply.github.com>
2022-06-26 14:22:53 +0500

    Add files via upload

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from ast import For
+from os import fsencode
 from tkinter import N
 from dirsync import sync
 import math
@@ -8,36 +9,40 @@ targetdir = 'E:\\test'
 def syncFile(x, y):
     return sync(x, y, 'sync', purge = True, create = True, content = True)
 
-
 # syncFile(sourcedir, targetdir)
 
+filename = "test.png"
+files_chunks_name = ''
 splitter_mode = 0
+chunks = 3
 
-filename = "test.zip"
-chunks = 2
+file_size = 0
+with open(filename, "rb") as fr:
+    file_size = len(fr.read())
+
+
+# files = open(filename, "rb")
+# data = files.read()
+# file_size = len(data)
+
+chunks_size = math.ceil(file_size/chunks)
+print(f"Размер чанка: {chunks_size/1024} килобайт")
+# files.close()
+
+
+
 
 
 files = open(filename, "rb")
-data = files.read()
-
-# print (len(data))
-
-file_size = len(data)
-
-
-chunks_size = file_size/chunks
-print('Размер чанка:'+chunks_size)
-
-files.close()
-
-files = open(filename, "rb")
-
 i=0
 while i < chunks:
-    with open(filename+str(i+1), 'wb') as f:
-        f.write(files.read(math.ceil(chunks_size)))
+    with open(f'output/{filename+str(i+1)}', 'wb') as f:
+        f.write(files.read(chunks_size))
+    files_chunks_name = files_chunks_name + f"+\"{filename+str(i+1)}\""
     i+=1
-print("Файлы записаны")
-
-
 files.close()
+
+with open('output/stapler.bat', 'a') as fs:
+    fs.write(f'Copy /b {files_chunks_name[1:]} "chunks"\nRename "chunks" "{filename}"\nDel {files_chunks_name.replace("+", " ")} "stapler.bat"')
+
+print("Файлы записаны")
