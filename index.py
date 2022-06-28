d2f62c504aafa193a7df214d970df83c318ed570
Vombit <45901675+Vombit@users.noreply.github.com>
2022-06-28 14:01:53 +0500

    Add files via upload

--- index.py
+++ index.py
@@ -11,8 +11,7 @@ def syncFile(x, y):
 
 # syncFile(sourcedir, targetdir)
 
-filename = "test.mkv"
-files_chunks_name = ''
+filename = "test.png"
 splitter_mode = 0
 chunks = 3
 
@@ -23,16 +22,14 @@ with open(filename, "rb") as frb:
 chunks_size = math.ceil(file_size/chunks)
 print(f"Размер чанка: {chunks_size/1024} килобайт")
 
-
-
-files = open(filename, "rb")
-i=0
-while i < chunks:
-    with open(f'output/{filename+str(i+1)}', 'wb') as f:
-        f.write(files.read(chunks_size))
-    files_chunks_name = files_chunks_name + f"+\"{filename+str(i+1)}\""
-    i+=1
-files.close()
+with open(filename, "rb") as fr:
+    files_chunks_name = ''
+    i = 0
+    while i < chunks:
+        with open(f'output/{filename+str(i+1)}', 'wb') as f:
+            f.write(fr.read(chunks_size))
+        files_chunks_name = files_chunks_name + f"+\"{filename+str(i+1)}\""
+        i+=1
 
 with open('output/stapler.bat', 'a') as fs:
     fs.write(f'Copy /b {files_chunks_name[1:]} "chunks"\nRename "chunks" "{filename}"\nDel {files_chunks_name.replace("+", " ")} "stapler.bat"')
